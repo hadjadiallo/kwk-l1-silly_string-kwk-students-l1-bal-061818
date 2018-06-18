e368b340c92f40796bf12fb77ccc7ad0f29a0440
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-18 20:26:21 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,7 +2,7 @@
 # Challenge 1: Fill this string in with your favorite word
 
 your_favorite_word = "food"
-puts "My favorite word is #{food}"
+puts "My favorite word is #{ food }"
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
